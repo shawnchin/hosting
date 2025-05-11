8b567a596d70f45dc383db812f1b53c28b77b296
Shawn <shawn@qwil.io>
2025-05-11 10:16:26 +0100

    nested iframe loader

---
+++ iframe-onload/arcade-loader.js
@@ -0,0 +1,31 @@
+let _arcadeLoaderConfig = undefined;
+
+function onLoadedCallback() {
+  // This func should be replaced by iframe parent before calling load()
+  console.log("Loaded");
+}
+
+function load(url) {
+  if (!_arcadeLoaderConfig) {
+    console.error('configureArcade() not called');
+    return
+  }
+
+  const iframe = document.createElement("iframe");
+  iframe.onload = onLoadedCallback;
+  iframe.width = '100%';
+  iframe.height = '100%';
+  iframe.src = _arcadeLoaderConfig.url;
+  document.getElementById(_arcadeLoaderConfig.containerId).appendChild(iframe);
+}
+
+
+function configureArcade(config) {
+  if (!config.url) {
+    console.error('"url" not set');
+  } else if (!config.containerId) {
+    console.error('"containerId" not set');
+  } else {
+    _arcadeLoaderConfig = config;
+  }
+}
